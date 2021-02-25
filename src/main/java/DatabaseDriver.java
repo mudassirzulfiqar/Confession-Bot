aee6ecea6ff9831cb7cb980fec2d19924cc0467b
Mudassir <mudaxxrex@gmail.com>
2021-02-25 23:14:35 +0500

    - refresh

--- src/main/java/DatabaseDriver.java
+++ src/main/java/DatabaseDriver.java
@@ -5,30 +5,8 @@ import java.sql.*;
 public class DatabaseDriver {
 
     public static void main(String[] args) throws Exception {
-
-        Connection connection = getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-        while (rs.next()) {
-            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
-        }
+        DatabaseHelper.getInstance().test();
     }
 
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-        System.out.println(username);
-        System.out.println(password);
-        String s = dbUrl + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-        System.out.println(s);
-        return DriverManager.getConnection(s, username, password);
-    }
 
 }
